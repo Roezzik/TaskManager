4b825de5295b7fb7bc9ecada306323ca18b1abc0
Roezzik <briniov.maksim@yandex.ru>
2020-12-02 13:53:27 +0400

    Add IdGenerator. Close #3

---
+++ src/main/java/task/manager/controllers/IdGenerator.java
@@ -0,0 +1,19 @@
+package task.manager.controllers;
+
+
+public class IdGenerator {
+	
+	private int counter;
+	
+	public IdGenerator() {
+		counter = 0;
+	}
+	
+	public IdGenerator(int counter) {
+		this.counter = counter;
+	}
+	
+	public int getNextId() {
+		return ++counter;
+	}
+}
