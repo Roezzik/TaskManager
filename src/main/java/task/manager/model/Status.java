456c211657d9d923e9ca9d62711ddb3d26e20b8f
Roezzik <briniov.maksim@yandex.ru>
2020-12-07 15:03:17 +0400

    Changed the indent by 4 spaces.

--- src/main/java/task/manager/model/Status.java
+++ src/main/java/task/manager/model/Status.java
@@ -2,20 +2,19 @@ package task.manager.model;
 
 
 public enum Status {
-
+    
     SCHEDULED("Scheduled"),
     DONE("Done"),
     POSTPONED("Postponed"),
     EXPIRED("Expired"),
     CANCELLED("Cancelled");
-
-    // what is title for this class? you can set Status.DONE or smthg else for tasks WO this field
+    
     private final String title;
-
+    
     Status(String title) {
         this.title = title;
     }
-
+    
     public String getTitle() {
         return title;
     }
--- src/main/java/task/manager/model/Task.java
+++ src/main/java/task/manager/model/Task.java
@@ -6,13 +6,13 @@ import java.util.Objects;
 
 
 public class Task {
-
-    private final int id;
-    private String name;
-    private String description;
-    private Date date;
-    private Status status;
-
+    
+    private final int    id;
+    private       String name;
+    private       String description;
+    private       Date   date;
+    private       Status status;
+    
     public Task(int id,
                 String name,
                 String description,
@@ -24,68 +24,68 @@ public class Task {
         this.date = date;
         this.status = status;
     }
-
+    
     public int getId() {
         return id;
     }
-
+    
     public String getName() {
         return name;
     }
-
+    
     public void setName(String name) {
         this.name = name;
     }
-
+    
     public String getDescription() {
         return description;
     }
-
+    
     public void setDescription(String description) {
         this.description = description;
     }
-
+    
     public Date getDate() {
         return date;
     }
-
+    
     public void setDate(Date date) {
         this.date = date;
     }
-
+    
     public Status getStatus() {
         return status;
     }
-
+    
     public void setStatus(Status status) {
         this.status = status;
     }
-
+    
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Task task = (Task) o;
         return id == task.id &&
-                name.equals(task.name) &&
-                description.equals(task.description) &&
-                date.equals(task.date) &&
-                status == task.status;
+               name.equals(task.name) &&
+               description.equals(task.description) &&
+               date.equals(task.date) &&
+               status == task.status;
     }
-
+    
     @Override
     public int hashCode() {
         return Objects.hash(id, name, description, date, status);
     }
-
+    
     @Override
     public String toString() {
         return "Task{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", description='" + description + '\'' +
-                ", date=" + date +
-                ", status=" + status +
-                '}';
+               "id=" + id +
+               ", name='" + name + '\'' +
+               ", description='" + description + '\'' +
+               ", date=" + date +
+               ", status=" + status +
+               '}';
     }
 }
