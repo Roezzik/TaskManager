11cb0fffd6458c1283f56f31ad32539e339b9655
Roezzik <briniov.maksim@yandex.ru>
2020-12-07 15:01:37 +0400

    Added Main class for start application.

--- src/main/java/task/manager/Main.java
+++ src/main/java/task/manager/Main.java
@@ -1,9 +1,29 @@
 package task.manager;
 
 
-public class Main {
-	public static void main(String[] args) {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-		System.out.println("Hi");
-	}
+import java.io.File;
+import java.net.URL;
+
+
+public class Main extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        URL    url  = new File("src/main/java/task/manager/view/mainForm/mainFormView.fxml").toURI().toURL();
+        Parent root = FXMLLoader.load(url);
+        primaryStage.setTitle("Task Manager");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+    
+    public static void main(String[] args) {
+        
+        launch(args);
+    }
 }
