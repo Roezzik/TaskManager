60739c26fcaa0485db4a637862bf8b9909e538d3
Roezzik <briniov.maksim@yandex.ru>
2020-12-14 20:08:34 +0400

    Added start part of the controller

--- src/main/java/task/manager/controller/Controller.java
+++ src/main/java/task/manager/controller/Controller.java
@@ -5,20 +5,35 @@ import task.manager.model.Journal;
 import task.manager.model.Task;
 
 import java.io.IOException;
+import java.util.List;
+
 
 public class Controller {
-    private Journal journal;
-
-
-    public Controller() throws IOException {
+    
+    private static Controller instance;
+    private final  Journal    journal;
+    
+    private Controller() throws IOException {
+        //journal = new Journal();
         journal = TextMarshaller.getInstance().read(Setting.getPropertyValue("FILE_PATH"));
+    }
+    
+    public static synchronized Controller getInstance() throws IOException {
+        if (instance == null) {
+            instance = new Controller();
+        }
+        return instance;
     }
 
     public void addTask(Task task) {
-        this.journal.addTask(task);
+        journal.addTask(task);
     }
 
-    public Journal get() {
+    public Journal getJournal() {
         return journal;
+    }
+    
+    public List<Task> getAllTasks() {
+        return journal.getListAllTasks();
     }
 }
