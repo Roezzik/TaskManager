375e796be6f18bf45a1f7483c5ae24afd5cf5430
Roezzik <briniov.maksim@yandex.ru>
2020-12-15 02:36:55 +0400

    Added constants for working with views

---
+++ src/main/java/task/manager/view/ViewOtherConstants.java
@@ -0,0 +1,10 @@
+package task.manager.view;
+
+
+public class ViewOtherConstants {
+    public static final String PATH_TO_EDIT_BUTTON_IMAGE = "src/main/resources/images/editButton.png";
+    public static final String STYLE_FOR_EDIT_BUTTON     = "-fx-background-color: transparent;";
+    public static final double MIN_SIZE_EDIT_BUTTON  = 17;
+    public static final double MAX_SIZE_EDIT_BUTTON  = 17;
+    public static final double PREF_SIZE_EDIT_BUTTON = 17;
+}
---
+++ src/main/java/task/manager/view/ViewPathConstants.java
@@ -0,0 +1,9 @@
+package task.manager.view;
+
+
+public class ViewPathConstants {
+    public static final String PATH_TO_MAIN_FORM_VIEW = "/mainForm/mainFormView.fxml";
+    public static final String PATH_TO_ADD_FORM_VIEW = "/addForm/addTaskView.fxml";
+    public static final String PATH_TO_EDIT_FORM_VIEW = "/editForm/editTaskView.fxml";
+    //public static final String PATH_TO_NOTIFICATION_FORM_VIEW = "/editForm/editTaskView.fxml";
+}
