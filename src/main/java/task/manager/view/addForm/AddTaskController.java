ed67005fddc888473f276227f04083aaedfb59ef
Roezzik <briniov.maksim@yandex.ru>
2020-12-15 02:34:12 +0400

    Changed the edit form similar to the add form, the functionality is waiting for the controller

--- src/main/java/task/manager/view/addForm/AddTaskController.java
+++ src/main/java/task/manager/view/addForm/AddTaskController.java
@@ -5,32 +5,18 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.stage.Stage;
-import task.manager.controller.Controller;
-import task.manager.controller.Setting;
-import task.manager.controller.io.TextMarshaller;
-import task.manager.model.Journal;
-import task.manager.model.Task;
 
 import java.io.IOException;
-import java.sql.Date;
-import java.sql.Timestamp;
-import java.text.DateFormat;
-import java.text.ParseException;
 import java.time.LocalDate;
-
-import static task.manager.model.Status.SCHEDULED;
 
 
 public class AddTaskController {
 
     @FXML
-    public Button add;
-
-    @FXML
     private TextField taskName;
     
     @FXML
-    private TextField taskDescription;
+    private TextArea taskDescription;
     
     @FXML
     private DatePicker notificationDate;
@@ -40,11 +26,16 @@ public class AddTaskController {
     
     @FXML
     private Spinner<Integer> notificationMinute;
-
-
-
+    
     @FXML
-    private void initialize() throws IOException {
+    public Button addButton;
+    
+    @FXML
+    public Button cancelButton;
+    
+    @FXML
+    private void initialize() {
+        
         initDatePicker();
         initSpinner();
     }
@@ -74,31 +65,45 @@ public class AddTaskController {
         notificationHour.setValueFactory(hourFactory);
         notificationMinute.setValueFactory(minuteFactory);
     }
-
-    public void clickadd(ActionEvent actionEvent) throws ParseException, IOException {
-        if (taskName.getText().equals("") || taskDescription.getText().equals("")) {
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setTitle("Add eroor");
-            alert.setHeaderText(null);
-            alert.setContentText("\n" + "Fill in all the fields");
-            alert.showAndWait();
-        } else {
-            String name = taskName.getText();
-            String description = taskDescription.getText();
-            Date date = Date.valueOf(notificationDate.getValue());
-            Timestamp timestamp = new Timestamp(date.getTime());
-            timestamp.setHours(notificationHour.getValue());
-            timestamp.setMinutes(notificationMinute.getValue());
-            System.out.println(timestamp);
-            Task task = new Task((int)(Math.random()*100), name, description, timestamp, SCHEDULED);
-            System.out.println(Controller.getInstance());
-            Controller.addTask(task);
-            System.out.println(Controller.getInstance());
-            Stage stage = (Stage) add.getScene().getWindow();
-            stage.close();
-        }
+    
+    public void clickCancelButton(ActionEvent event) {
+        Stage stage = (Stage) cancelButton.getScene().getWindow();
+        stage.close();
     }
 
+//    public void clickadd(ActionEvent actionEvent) throws ParseException, IOException {
+//        if (taskName.getText().equals("") || taskDescription.getText().equals("")) {
+//            Alert alert = new Alert(Alert.AlertType.ERROR);
+//            alert.setTitle("Add eroor");
+//            alert.setHeaderText(null);
+//            alert.setContentText("\n" + "Fill in all the fields");
+//            alert.showAndWait();
+//        } else {
+//            String name = taskName.getText();
+//            String description = taskDescription.getText();
+//            Date date = Date.valueOf(notificationDate.getValue());
+//            Timestamp timestamp = new Timestamp(date.getTime());
+//            timestamp.setHours(notificationHour.getValue());
+//            timestamp.setMinutes(notificationMinute.getValue());
+//            System.out.println(timestamp);
+//            Task task = new Task(1, name, description, timestamp, SCHEDULED);
+//            controller.addTask(task);
+//            Stage stage = (Stage) add.getScene().getWindow();
+//            stage.close();
+//        }
+//    }
+    
+//    @FXML
+//    private void addTask(ActionEvent event) {
+//        LocalDate taskDate = notificationDate.getValue();
+//        Date date = Date.from(taskDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
+//        Task newTask = new Task(IdGenerator.getIdGenerator().getNextId(), taskName.getText(), taskDescription.getText(), date, Status.SCHEDULED);
+//        System.out.println(newTask);
+//
+//        MainFormController controller = fxmlLoader.getController();
+//        controller.setJournal(journal);
+//        Stage stage = (Stage) addButton.getScene().getWindow();
+//        stage.close();
+//    }
 
-    //TODO
 }
--- src/main/java/task/manager/view/editForm/EditTaskController.java
+++ src/main/java/task/manager/view/editForm/EditTaskController.java
@@ -15,63 +15,62 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.ZoneId;
 
-import static task.manager.model.Status.SCHEDULED;
 
 public class EditTaskController {
-
-    public TextField name;
     
-    public TextField desc;
-    
-    public DatePicker date;
-    
-    public Button edit;
-    
-    public Button cancel;
-
     @FXML
-    private void initialize() throws IOException {
-        initDatePicker();
-    }
-
+    private TextField taskName;
+    
     @FXML
-    private void initDatePicker() {
-        LocalDate today = LocalDate.now();
-        date.setValue(today);
-        date.setDayCellFactory(picker -> new DateCell() {
-            public void updateItem(LocalDate date, boolean empty) {
-                super.updateItem(date, empty);
-
-                setDisable(empty || date.compareTo(today) < 0);
-            }
-        });
+    private TextArea taskDescription;
+    
+    @FXML
+    private DatePicker notificationDate;
+    
+    @FXML
+    private Spinner<Integer> notificationHour;
+    
+    @FXML
+    private Spinner<Integer> notificationMinute;
+    
+    @FXML
+    public Button saveButton;
+    
+    @FXML
+    public Button cancelButton;
+    
+    @FXML
+    private void initialize() {
+    
     }
-    public void showtask(Task task){
-        name.setText(task.getName());
-        desc.setText(task.getDescription());
-        date.setValue(task.getDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate());
-    }
-
-    public void clickedit() throws IOException {
-        if (name.getText().equals("") || desc.getText().equals("")) {
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setTitle("Add eroor");
-            alert.setHeaderText(null);
-            alert.setContentText("\n" + "Fill in all the fields");
-            alert.showAndWait();
-        } else {
-            String names = name.getText();
-            String description = desc.getText();
-            Date data = Date.valueOf(date.getValue());
-            //task = new Task((int)(Math.random()*100), names, description, data, SCHEDULED);
-            //Controller.addTask(task);
-            Stage stage = (Stage) edit.getScene().getWindow();
-            stage.close();
-        }
-    }
-
-    public void clickcancel(ActionEvent actionEvent) {
-        Stage stage = (Stage) cancel.getScene().getWindow();
+    
+    public void clickCancelButton(ActionEvent event) {
+        Stage stage = (Stage) cancelButton.getScene().getWindow();
         stage.close();
     }
+    
+//    public void showtask(Task task) {
+//        name.setText(task.getName());
+//        desc.setText(task.getDescription());
+//        date.setValue(task.getDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate());
+//    }
+
+//    public void clickedit() throws IOException {
+//        if (name.getText().equals("") || desc.getText().equals("")) {
+//            Alert alert = new Alert(Alert.AlertType.ERROR);
+//            alert.setTitle("Add eroor");
+//            alert.setHeaderText(null);
+//            alert.setContentText("\n" + "Fill in all the fields");
+//            alert.showAndWait();
+//        } else {
+//            String names       = name.getText();
+//            String description = desc.getText();
+//            Date   data        = Date.valueOf(date.getValue());
+//            //task = new Task((int)(Math.random()*100), names, description, data, SCHEDULED);
+//            //Controller.addTask(task);
+//            Stage stage = (Stage) edit.getScene().getWindow();
+//            stage.close();
+//        }
+//    }
+
 }
