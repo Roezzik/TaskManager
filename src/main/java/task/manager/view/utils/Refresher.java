ed2ac43d844eebf057f31e51df38810f5998b0b3
Roezzik <briniov.maksim@yandex.ru>
2020-12-22 03:05:03 +0400

    Implemented the Refresher class for updating tables outside the MainFormController

---
+++ src/main/java/task/manager/view/utils/Refresher.java
@@ -0,0 +1,29 @@
+package task.manager.view.utils;
+
+
+import task.manager.view.mainForm.MainFormController;
+
+
+public class Refresher {
+    
+    private static Refresher          instance;
+    private        MainFormController mainFormController;
+    
+    private Refresher() {
+    }
+    
+    public static Refresher getInstance() {
+        if (instance == null) {
+            instance = new Refresher();
+        }
+        return instance;
+    }
+    
+    public MainFormController getMainFormController() {
+        return mainFormController;
+    }
+    
+    public void setMainFormController(MainFormController mainFormController) {
+        this.mainFormController = mainFormController;
+    }
+}
