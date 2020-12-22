93f990676475c05ab5f2db9383f178db40245547
Roezzik <briniov.maksim@yandex.ru>
2020-12-22 15:57:49 +0400

    Return remote JournalFactory

---
+++ src/main/java/task/manager/controller/factory/JournalFactory.java
@@ -0,0 +1,12 @@
+package task.manager.controller.factory;
+
+
+import task.manager.model.Journal;
+
+
+public class JournalFactory {
+    
+    public Journal create() {
+        return new Journal();
+    }
+}
