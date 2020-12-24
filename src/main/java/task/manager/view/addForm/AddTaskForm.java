77eb67bc6fcbce2e93cf7f5f222b931323ed7785
Roezzik <briniov.maksim@yandex.ru>
2020-12-25 00:12:28 +0400

    Blocking the MainForm if this window is active

--- src/main/java/task/manager/view/addForm/AddTaskForm.java
+++ src/main/java/task/manager/view/addForm/AddTaskForm.java
@@ -4,6 +4,7 @@ package task.manager.view.addForm;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import task.manager.view.utils.ViewConstants;
 import task.manager.view.utils.ViewPathConstants;
@@ -12,6 +13,7 @@ import task.manager.view.utils.ViewPathConstants;
 public class AddTaskForm {
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource(ViewPathConstants.PATH_TO_ADD_FORM_VIEW));
+        stage.initModality(Modality.APPLICATION_MODAL);
         stage.setTitle(ViewConstants.TITLE_TO_ADD_FORM_VIEW);
         stage.setScene(new Scene(root));
         stage.show();
--- src/main/java/task/manager/view/editForm/EditTaskForm.java
+++ src/main/java/task/manager/view/editForm/EditTaskForm.java
@@ -4,6 +4,7 @@ package task.manager.view.editForm;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import task.manager.view.utils.ViewConstants;
 import task.manager.view.utils.ViewPathConstants;
@@ -12,6 +13,7 @@ import task.manager.view.utils.ViewPathConstants;
 public class EditTaskForm {
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource(ViewPathConstants.PATH_TO_EDIT_FORM_VIEW));
+        stage.initModality(Modality.APPLICATION_MODAL);
         stage.setTitle(ViewConstants.TITLE_TO_EDIT_FORM_VIEW);
         stage.setScene(new Scene(root));
         stage.show();
