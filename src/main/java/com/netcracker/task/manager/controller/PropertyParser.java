ee89e0c65e033cfb8e35468b2c871e3f36241990
Leonid Galushka <leonid.wf@mail.ru>
2020-12-28 22:54:37 +0400

    fix bag to TextMarshaller

--- src/main/java/com/netcracker/task/manager/controller/PropertyParser.java
+++ src/main/java/com/netcracker/task/manager/controller/PropertyParser.java
@@ -10,23 +10,51 @@ import java.util.Properties;
 
 
 public class PropertyParser {
-    
+
+    private String fileFormat;
+
+    private String pathToBackup;
+
+///
+    public String getFileFormat() throws PropertyReadException {
+
+        if (fileFormat == null) {
+            fileFormat = getPropertyValue("backup_format");
+        }
+
+        return fileFormat;
+    }
+
+    public String getPathToBackup() throws PropertyReadException {
+        if (pathToBackup == null) {
+            if (fileFormat == null) {
+                getFileFormat();
+            }
+            if (fileFormat.equals("txt"))
+                pathToBackup = getPropertyValue("path_to_bin_backup");
+            if (fileFormat.equals("bin"))
+                pathToBackup = getPropertyValue("path_to_txt_backup");
+        }
+        return pathToBackup;
+    }
+///
+
     public static final String PATH_TO_PROPERTIES = "staff/application.properties";
-    
-    public static String getPropertyValue(String PropertyName) throws PropertyReadException {
-        
+
+    public static String getPropertyValue(String propertyName) throws PropertyReadException {
+
         Properties properties = new Properties();
-        String     propertyValue;
-        
+        String propertyValue;
+
         try (InputStream inputStream = new FileInputStream(PATH_TO_PROPERTIES)) {
             properties.load(inputStream);
-            propertyValue = properties.getProperty(PropertyName);
+            propertyValue = properties.getProperty(propertyName);
         } catch (IOException e) {
             throw new PropertyReadException(ViewConstants.ERROR_READ_PROPERTY);
         }
         return propertyValue;
     }
-    
+
     public static int getIntegerPropertyValue(String PropertyName) throws NumberFormatException, PropertyReadException {
         return Integer.parseInt(getPropertyValue(PropertyName));
     }
--- src/main/java/com/netcracker/task/manager/controller/io/TextMarshaller.java
+++ src/main/java/com/netcracker/task/manager/controller/io/TextMarshaller.java
@@ -15,32 +15,33 @@ import com.netcracker.task.manager.view.utils.AlertForm;
 import com.netcracker.task.manager.view.utils.ViewConstants;
 
 import java.io.*;
+import java.util.Date;
 
 
 public class TextMarshaller implements Marshaller {
-    
+
     private static TextMarshaller instance;
-    
+
     private static final String PATH_TO_TXT_BACKUP = "path_to_txt_backup";
-    
+
     private TextMarshaller() {
     }
-    
+
     public static TextMarshaller getInstance() {
         if (instance == null) {
             instance = new TextMarshaller();
         }
         return instance;
     }
-    
+
     @Override
     public Journal read()
-    throws CreateFileException, TextMarshallerReadException, IOException, PropertyReadException,
-           BufferedReaderException {
-        
+            throws CreateFileException, TextMarshallerReadException, IOException, PropertyReadException,
+            BufferedReaderException {
+
         String pathToFile = PropertyParser.getPropertyValue(PATH_TO_TXT_BACKUP);
-        File   file       = new File(pathToFile);
-        
+        File file = new File(pathToFile);
+
         try {
             if (!file.exists()) {
                 boolean create = file.createNewFile();
@@ -49,27 +50,68 @@ public class TextMarshaller implements Marshaller {
         } catch (Exception e) {
             throw new CreateFileException(ViewConstants.ERROR_CREATE_FILE);
         }
-        
-        Journal        journal = new Journal();
+
+        Journal journal = new Journal();
         BufferedReader br;
         try {
             br = new BufferedReader(new FileReader(file));
         } catch (FileNotFoundException e) {
             throw new BufferedReaderException(ViewConstants.ERROR_BUFFER_READ_EXCEPTION);
         }
-        
+
         String str;
+        String str2;
+
         while ((str = br.readLine()) != null) {
-            
+
+            // отступы между тасками
             if (str.equals("")) continue;
-            
+
             try {
-                Task task = new Task(Integer.parseInt(str),
-                                     br.readLine(),
-                                     br.readLine(),
-                                     DateConverter.stringToDate(br.readLine()),
-                                     null);
-                switch (br.readLine()) {
+                int sum = 0;
+                int idTask = 0;
+                String nameTask = "";
+                String descriptionTask = "";
+                Date dateTask = null;
+
+                idTask = Integer.parseInt(str);
+                while (true) {
+                    str2 = br.readLine();
+
+                    if (sum == 0) {
+                        nameTask = str2;
+                    }
+                    // description tasks
+                    if (sum == 1) {
+                        String str3 = "";
+                        if (str2.equals("")) {
+                            str3 = "";
+                        } else {
+                            str3 = str2;
+                        }
+                        while (true) {
+                            str2 = br.readLine();
+                            if (str2.equals("")) {
+                                str3 += '\n' + "";
+                            } else {
+                                try {
+                                    dateTask = DateConverter.stringToDate(str2);
+                                    descriptionTask = str3;
+                                    break;
+                                } catch (Exception e) {
+                                    str3 += '\n' + str2;
+                                }
+                            }
+                        }
+                    }
+                    if (sum == 2) {
+                        break;
+                    }
+                    sum++;
+                }
+                Task task = new Task(idTask, nameTask, descriptionTask, dateTask, null);
+
+                switch (str2) {
                     case "Scheduled" -> task.setStatus(Status.SCHEDULED);
                     case "Done" -> task.setStatus(Status.DONE);
                     case "Postponed" -> task.setStatus(Status.POSTPONED);
@@ -83,19 +125,19 @@ public class TextMarshaller implements Marshaller {
         }
         return journal;
     }
-    
+
     @Override
     public void write(Journal journal) throws CreateFileException, PropertyReadException, PrintWriterException {
-        
+
         String pathToFile = PropertyParser.getPropertyValue(PATH_TO_TXT_BACKUP);
-        File   file       = new File(pathToFile);
-        
+        File file = new File(pathToFile);
+
         try {
             boolean create = file.createNewFile();
         } catch (IOException e) {
             throw new CreateFileException(ViewConstants.ERROR_CREATE_FILE);
         }
-        
+
         try (PrintWriter pw = new PrintWriter(file)) {
             journal.getListAllTasks().forEach(task -> {
                 pw.println(task.getId());
@@ -108,6 +150,6 @@ public class TextMarshaller implements Marshaller {
         } catch (FileNotFoundException fileNotFoundException) {
             throw new PrintWriterException(ViewConstants.ERROR_PRINT_WRITER_EXCEPTION);
         }
-        
+
     }
 }
