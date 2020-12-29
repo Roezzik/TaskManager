942c2a5aec24546b4458cab9544288ab1c88e555
Leonid Galushka <leonid.wf@mail.ru>
2020-12-29 23:02:14 +0400

    javaDoc property

--- src/main/java/com/netcracker/task/manager/controller/PropertyParser.java
+++ src/main/java/com/netcracker/task/manager/controller/PropertyParser.java
@@ -9,9 +9,14 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
-
+/**
+ * class for working with  Property file
+ */
 public class PropertyParser {
-    
+
+    /**
+     * PATH_TO_PROPERTIES -  path to the property file
+     */
     public final   String         PATH_TO_PROPERTIES = "staff/application.properties";
     private static PropertyParser instance;
     private final  Properties     properties;
@@ -32,7 +37,12 @@ public class PropertyParser {
         }
         return instance;
     }
-    
+
+    /**
+     * getPropertyValue is to return the value of the variable by its name from the property file
+     * @param propertyName - the name of the variable
+     * @return the value of the variable by its name from the property file
+     */
     public String getPropertyValue(String propertyName) {
         return properties.getProperty(propertyName);
     }
