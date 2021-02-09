2b3d20d152d3aca228d00ff2837aec96aada78fd
Roezzik <briniov.maksim@yandex.ru>
2021-02-09 19:11:44 +0400

    Added classes with client and server command constants

---
+++ src/main/java/shared/constant/ClientCommandConstants.java
@@ -0,0 +1,7 @@
+package shared.constant;
+
+
+public class ClientCommandConstants {
+    public final static int GET_ALL_TASKS     = 10;
+    public final static int SHOW_NOTIFICATION = 11;
+}
---
+++ src/main/java/shared/constant/ServerCommandConstants.java
@@ -0,0 +1,12 @@
+package shared.constant;
+
+
+public class ServerCommandConstants {
+    public final static int SEND_TASKS        = 0;
+    public final static int ADD_TASK          = 1;
+    public final static int UPDATE_TASK       = 2;
+    public final static int DELETE_TASK       = 3;
+    public final static int CANCEL_TASK       = 4;
+    public final static int DONE_TASK         = 5;
+    public final static int DISCONNECT_CLIENT = 6;
+}
